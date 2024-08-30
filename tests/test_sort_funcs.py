bba4a10b89b1b469c1bfcf80cbc599d4c3712d3e
Jianping <pinkwjp@gmail.com>
2024-08-30 13:12:16 -0400

    created file and add test

--- tests/test_sort_funcs.py
+++ tests/test_sort_funcs.py
@@ -4,8 +4,7 @@ from random import randint
 from src.sort_funcs import bubble_sort
 
 def test_bubble_sort():
-    nums = [randint(1, 20)] * 5
-    sorted_nums = sorted(nums)
-    bubble_sort_results = [bubble_sort(nums)]
+    nums = [randint(1, 20) for _ in range(5)]
+    bubble_sort_results = [partial_result for partial_result in bubble_sort(nums)]
     bubble_sorted_nums = bubble_sort_results[-1]
-    assert sorted_nums == bubble_sorted_nums
+    assert bubble_sorted_nums == sorted(nums)
