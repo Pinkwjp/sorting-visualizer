98e116de3e0f56a0bbe1561070a91e993d2a13f5
Jianping <pinkwjp@gmail.com>
2024-08-30 13:35:10 -0400

    add test

--- tests/test_sort_funcs.py
+++ tests/test_sort_funcs.py
@@ -1,10 +1,24 @@
 # pytest tests/test_sort_funcs.py
 
 from random import randint
-from src.sort_funcs import bubble_sort
+from typing import List
 
-def test_bubble_sort():
-    nums = [randint(1, 20) for _ in range(5)]
-    bubble_sort_results = [partial_result for partial_result in bubble_sort(nums)]
-    bubble_sorted_nums = bubble_sort_results[-1]
-    assert bubble_sorted_nums == sorted(nums)
+from src.sort_funcs import (gen_bubble_sort, gen_insertion_sort, 
+                            gen_merge_sort, gen_quicksort)
+
+
+
+def final_sorted_result(sort_func, nums) -> List[int]:
+    partial_sorted_results = [partial_result for partial_result in sort_func(nums)]
+    final_result = partial_sorted_results[-1]
+    return final_result
+
+
+class TestSortFuncs:
+    nums = [randint(1, 20) for _ in range(10)]
+    sorted_nums = sorted(nums)
+    sort_funcs = [gen_bubble_sort, gen_insertion_sort, gen_merge_sort, gen_quicksort]
+
+    def test_all_sort_funcs(self):
+        for f in self.sort_funcs:
+            assert final_sorted_result(f, self.nums) == self.sorted_nums
